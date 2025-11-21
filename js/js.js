e8599c92877fbf6f52b4de7a72fcaa291fec5641
abp0035 <abp0035@alu.medac.es>
2025-11-21 18:48:09 +0100

    ...

--- js/js.js
+++ js/js.js
@@ -10,3 +10,27 @@ boton.addEventListener('click', () => {
         boton.textContent = '▶ Música';
     }
 });
+
+
+const slider = document.querySelector('.slider');
+const slidesContainer = slider.querySelector('.slides');
+const slides = slider.querySelectorAll('.slide');
+
+let index = 0;
+function actualizarSlide() {
+    const slideWidth = slides[0].clientWidth;
+    slidesContainer.style.transform = `translateX(-${index * slideWidth}px)`;
+}
+
+function siguienteSlide() {
+    index = (index + 1) % slides.length;
+    actualizarSlide();
+}
+
+window.addEventListener('load', actualizarSlide);
+
+// Para que no se rompa al redimensionar la ventana
+window.addEventListener('resize', actualizarSlide);
+
+// Cambio de slider cada 4s
+setInterval(siguienteSlide, 4000);
